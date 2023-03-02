894ce88c1ef0c9b820bfc3a93d9f801db0fbeaa6
manuelnones <manuelnones99@yahoo.com>
2023-03-02 15:46:18 +0100

    pseudocode ex 1

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// chiedere all' utente di inserire una parola
+// creare una funzione per capire se la parola inserita è palindroma
+
+/*
+    - creare una variabile dell input html
+    - creare una variabile del button html
+    - creare la funzione per la verifica della parola palindroma
+   SE la parola è palindroma
+    o dire all' utente che la parola è palindroma
+ALTRIMENTI
+    o dire all utente che la parola non è palindroma
+*/
